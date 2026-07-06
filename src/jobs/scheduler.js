e14ff59a1a57f4280aba0f392bec024b79e3c0c4
FyyFzn <rafifauzanhermansah@gmail.com>
2026-07-07 04:30:27 +0700

    refactor: revert startup stagger delays to original fast settings

--- src/jobs/scheduler.js
+++ src/jobs/scheduler.js
@@ -15,7 +15,7 @@ export function initScheduler() {
     startBackgroundLatestSync();
     startBackgroundKuronimeSync();
 
-    // 2. Memulai proses unified sync & neosatsu (delay 10 detik agar server siap menerima request)
+    // 2. Memulai proses unified sync (dijadwalkan setelah 10 detik agar server stabil)
     setTimeout(() => {
         const runUnifiedLoop = async () => {
             let hasMore = false;
@@ -36,7 +36,7 @@ export function initScheduler() {
             setTimeout(runNeosatsuLoop, 604800000);
         };
         runNeosatsuLoop();
-    }, 60000);
+    }, 10000);
 
     // 3. Pembersihan file sampah temporer (Garbage Collection) setiap 12 jam
     import('fs').then(fs => {
