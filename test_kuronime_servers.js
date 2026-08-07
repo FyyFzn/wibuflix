3ca69dc33c2e8d89aef014866943538c57104c1a
FyyFzn <rafifauzanhermansah@gmail.com>
2026-08-07 07:02:33 +0700

    chore: remove test scripts

--- test_kuronime_servers.js
+++
@@ -1,7 +0,0 @@
-import * as kuronime from './src/services/scrapers/kuronimeScraper.js';
-
-kuronime.getKuronimeServers('https://kuronime.sbs/nonton-otome-kaijuu-carameliser-episode-6/')
-    .then(r => {
-        console.log(JSON.stringify(r, null, 2));
-        process.exit(0);
-    });
--- test_redirect.js
+++
@@ -1,6 +0,0 @@
-fetch('https://kuronime.sbs/nonton-otome-kaijuu-carameliser-episode-6/', { redirect: 'manual' })
-    .then(r => {
-        console.log("Status:", r.status);
-        console.log("Location Header:", r.headers.get('location'));
-    })
-    .catch(e => console.error(e));
